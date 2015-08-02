6b3caf72201f05acfe4c232e690e01d93e835b2c
akry <a-yokokawa@axsh.net>
2015-08-03 01:17:50 +0900

    Stub test scenario for wanedge

---
+++ integration_test/lib/vnspec/spec/wanedge_spec.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+require_relative "spec_helper"
+
+describe "wanedge" do
+  describe "vm1" do
+    it "reachable to the internet(8.8.8.8)" do
+      true
+    end
+  end
+end
